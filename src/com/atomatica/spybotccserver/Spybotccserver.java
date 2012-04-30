ba8afd46a8eac7d119bc856f637e97604c16a226
Alex Leigh <leigh@alexleigh.me>
2012-04-30 13:28:49 -0400

    Implementing more robust protocol.

--- src/com/atomatica/spybotccserver/Spybotccserver.java
+++ src/com/atomatica/spybotccserver/Spybotccserver.java
@@ -2,15 +2,19 @@ package com.atomatica.spybotccserver;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.apache.commons.daemon.*;
 
 public class Spybotccserver implements Daemon {
     protected int port = 9103;
+    protected String protocolHeader = "SPYBOTCCP/1.0";
     protected ServerSocket serverSocket;
     protected Socket clientSocket;
     protected ExecutorService requestHandlerPool;
+    protected ArrayList<Spybot> spybots;
     
     // entry point using JSVC
     @Override
@@ -35,6 +39,7 @@ public class Spybotccserver implements Daemon {
         }
         
         requestHandlerPool = Executors.newCachedThreadPool();
+        spybots = new ArrayList<Spybot>();
     }
 
     @Override
@@ -81,84 +86,130 @@ public class Spybotccserver implements Daemon {
         spybotccserver.init(args);
         spybotccserver.start();
     }
-}
 
-class RequestHandler implements Runnable {
-    protected String clientName;
-    protected Socket clientSocket;
-    protected BufferedReader input;
-    protected PrintWriter output;
-    protected Thread thread;
-    
-    public RequestHandler(String clientName, Socket clientSocket) {
-        this.clientName = clientName;
-        this.clientSocket = clientSocket;
+    class RequestHandler implements Runnable {
+        protected String clientName;
+        protected Socket clientSocket;
+        protected BufferedReader input;
+        protected PrintWriter output;
+        protected Thread thread;
         
-        try {
-            output = new PrintWriter(clientSocket.getOutputStream(), true);
-            input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println("RequestHandler thread handling request from: " + clientName);
-        }
-        
-        catch (IOException e) {
-            System.err.println("Error getting I/O streams for client: " + clientName);
-        }
-    }
-    
-    @Override
-    public void run() {
-        synchronized(this) {
-            thread = Thread.currentThread();
-        }
-        
-        try {
-            String message = "";
-            output.println("SPYBOTCCP/1.0 " + InetAddress.getLocalHost().getHostName());
+        public RequestHandler(String clientName, Socket clientSocket) {
+            this.clientName = clientName;
+            this.clientSocket = clientSocket;
             
-            while (!thread.isInterrupted()) {
-                message = input.readLine();
-                System.out.println("Client " + clientName + "> " + message);
+            try {
+                output = new PrintWriter(clientSocket.getOutputStream(), true);
+                input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("RequestHandler thread handling request from: " + clientName);
+            }
+            
+            catch (IOException e) {
+                System.err.println("Error getting I/O streams for client: " + clientName);
+            }
+        }
+        
+        @Override
+        public void run() {
+            synchronized (this) {
+                thread = Thread.currentThread();
+            }
+            
+            try {
+                String message = "";
+                output.println(protocolHeader + " " + InetAddress.getLocalHost().getHostName());
                 
-                /*for (int i = 0; i < requestHandlerPool.length; i++) {
-                    if (handlers[i] != null & handlers[i] != this) {
-                        handlers[i].output.writeObject(message);
-                        handlers[i].output.flush();
+                while (!thread.isInterrupted()) {
+                    message = input.readLine();
+                    System.out.println(clientName + " - " + message);
+                    
+                    // parse message
+                    try {
+                        String[] tokens = message.split("\\s");
+                        
+                        if (tokens[0].equals("TERMINATE")) {
+                            break;
+                        }
+                        
+                        else if (tokens[0].equals("ANNOUNCE")) {
+                            String spybotName = tokens[1];
+                            synchronized (spybots) {
+                                // TODO: check if spybot name has already been used
+                                spybots.add(new Spybot(spybotName, this));
+                            }   
+                        }
+                        
+                        else if (tokens[0].equals("CONTROL")) {
+                            String spybotName = tokens[1];
+                            Spybot spybot = null;
+                            synchronized (spybots) {
+                                Iterator<Spybot> i = spybots.iterator();
+                                while (i.hasNext()) {
+                                    Spybot s = i.next();
+                                    if (s.name.equals(spybotName)) {
+                                        spybot = s;
+                                        break;
+                                    }
+                                }
+                                
+                                if (spybot == null) {
+                                    output.println(protocolHeader + "  300 INVALID");
+                                }
+                                
+                                else {
+                                    String passphrase = tokens[2];
+                                }
+                            }
+                        }
                     }
-                }*/
+                    
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        output.println(protocolHeader + "  300 INVALID");
+                    }
+                }
+            }
+            
+            catch (EOFException e) {
+                System.out.println("Client " + clientName + " closed connection");
+            }
+    
+            catch (IOException e) {
+                System.err.println("Error communicating with client: " + clientName);
+            }
+    
+            finally {
+                try {
+                    output.close();
+                    input.close();
+                    clientSocket.close();
+                    System.out.println("Closed socket with client: " + clientName);
+                }
                 
-                if (message.equals("TERMINATE")) {
-                    break;
+                catch(IOException e) {
+                    System.err.println("Error closing socket for client: " + clientName);
                 }
             }
         }
+    }
+    
+    class Spybot {
+        protected String name;
+        protected RequestHandler requestHandler;
+        protected Spybotcontroller controller;
         
-        catch (EOFException e) {
-            System.out.println("Client " + clientName + " closed connection");
+        public Spybot(String name, RequestHandler requestHandler) {
+            this.name = name;
+            this.requestHandler = requestHandler;
+            this.controller = null;
         }
-
-        catch (IOException e) {
-            System.err.println("Error communicating with client: " + clientName);
+        
+        public boolean login(String passphrase) {
+            requestHandler.output.println("login " + passphrase);
+            return false;
         }
-
-        finally {
-            try {
-                output.close();
-                input.close();
-                clientSocket.close();
-                System.out.println("Closed socket with client: " + clientName);
-            }
-            
-            catch(IOException e) {
-                System.err.println("Error closing socket for client: " + clientName);
-            }
-            
-            finally {
-                /*for (int i = 0; i < handlers.length; i++) {
-                    if (handlers[i] == this) {
-                        handlers[i] = null;
-                    }
-                }*/
-            }
-        }
+    }
+    
+    class Spybotcontroller {
+        
     }
 }
